774ea651b4de8216477cced67740e705955408c3
ErikaGillig <90275725+ErikaGillig@users.noreply.github.com>
2023-11-06 20:54:48 -0300

    3

--- app.js
+++ app.js
@@ -1,65 +1,77 @@
+// Importa los módulos necesarios
 const express = require("express");
-const app = express();
+const mongoose = require("mongoose");
 const expressHandlebars = require("express-handlebars");
 const WebSocket = require("ws");
-const fs = require("fs");
-const ProductManager = require("./ProductManager");
+const ProductManager = require("./ProductManager"); // Si aún es necesario
 
-// Configurar Handlebars como motor de plantillas
+// Crea una instancia de Express
+const app = express();
+
+// Conecta a la base de datos MongoDB
+mongoose.connect("mongodb://localhost:27017/tu_basedatos", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "Error de conexión a MongoDB"));
+db.once("open", () => {
+  console.log("Conexión a MongoDB exitosa");
+});
+
+// Configura Handlebars como motor de plantillas
 app.engine("hbs", expressHandlebars({ extname: "hbs" }));
 app.set("view engine", "hbs");
 app.set("views", __dirname + "/views");
 
-app.use(express.json());
+// Configura WebSocket
+const server = app.listen(PORT, () => {
+  console.log(`Servidor Express en ejecución en el puerto ${PORT}`);
+});
+const wss = new WebSocket.Server({ server });
 
-const dataFilePath = "products.json";
-const productManager = new ProductManager(dataFilePath);
+// Maneja conexiones WebSocket
+wss.on("connection", (ws) => {
+  console.log("Nueva conexión WebSocket");
 
-// Cargar los datos al iniciar la aplicación
-productManager.loadData()
-  .then(() => {
-    // Rutas para productos (ya definidas)
-
-    // Rutas para carritos (ya definidas)
-
-    // Resto del código de rutas (si hay más)
-
-    const PORT = process.env.PORT || 3000;
-
-    // Crear servidor HTTP
-    const server = app.listen(PORT, () => {
-      console.log(`Servidor Express en ejecución en el puerto ${PORT}`);
-    });
-
-    // Configurar WebSocket
-    const wss = new WebSocket.Server({ server });
-
-    // Manejar conexiones WebSocket
-    wss.on("connection", (ws) => {
-      console.log("Nueva conexión WebSocket");
-
-      // Maneja mensajes WebSocket entrantes
-      ws.on("message", (message) => {
-        console.log(`Mensaje recibido: ${message}`);
-        // Aquí puedes implementar la lógica para manejar los mensajes WebSocket
-      });
-
-      // Manejar cierre de conexión
-      ws.on("close", () => {
-        console.log("Conexión WebSocket cerrada");
-      });
-
-      // Resto de la configuración de WebSocket, como manejar eventos
-      // y comunicación en tiempo real con los clientes
-    });
-  })
-  .catch((error) => {
-    console.error("Error al cargar los datos:", error);
+  // Maneja mensajes WebSocket entrantes
+  ws.on("message", (message) => {
+    console.log(`Mensaje recibido: ${message}`);
+    // Aquí puedes implementar la lógica para manejar los mensajes WebSocket
   });
 
-// Ruta para mostrar la vista de productos en tiempo real
-app.get("/realtimeproducts", (req, res) => {
-  // Obtén la lista de productos (puedes obtenerla de tu ProductManager)
-  const products = productManager.getProducts();
-  res.render("realTimeProducts", { products });
+  // Maneja cierre de conexión
+  ws.on("close", () => {
+    console.log("Conexión WebSocket cerrada");
+  });
+
+  // Resto de la configuración de WebSocket, como manejar eventos
+  // y comunicación en tiempo real con los clientes
 });
+
+// Importa y configura el modelo de Producto (definido con Mongoose)
+const Product = require("./models/Product");
+
+// Carga las rutas de productos (ajusta según tus necesidades)
+const productRoutes = require("./routes/products");
+app.use("/api/products", productRoutes);
+
+// Carga las rutas de carritos (ajusta según tus necesidades)
+const cartRoutes = require("./routes/carts");
+app.use("/api/carts", cartRoutes);
+
+// Carga las rutas de vistas (ajusta según tus necesidades)
+const viewRoutes = require("./routes/views");
+app.use("/views", viewRoutes);
+
+// Agrega otras rutas y configuraciones si es necesario
+
+// Puerto donde se ejecutará el servidor
+const PORT = process.env.PORT || 3000;
+
+// Inicia el servidor
+app.listen(PORT, () => {
+  console.log(`Servidor Express en ejecución en el puerto ${PORT}`);
+});
+
