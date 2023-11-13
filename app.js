1742a044e568d9adb3e4713bdc7c13d39f5d92ee
ErikaGillig <90275725+ErikaGillig@users.noreply.github.com>
2023-11-13 08:03:41 -0300

    Update app.js

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const ProductManager = require("./ProductManager");
 const app = express();
 
 // Conecta a la base de datos MongoDB
-mongoose.connect("mongodb://localhost:27017/tu_basedatos", {
+mongoose.connect("mongodb://localhost:27017/CofeeDB", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
@@ -47,7 +47,7 @@ wss.on("connection", (ws) => {
     console.log("Conexión WebSocket cerrada");
   });
 
-  // Resto de la configuración de WebSocket, como manejar eventos
+  // Resto de la configuración de WebSocket, manejar eventos
   // y comunicación en tiempo real con los clientes
 });
 
@@ -77,11 +77,11 @@ app.listen(PORT, () => {
 });
   
 // Coneccion a DB 
-
+// user= dbUser_main password= dbusermain90
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = "mongodb+srv://dbUser_Main:<dbusermain90>@cofeedb.rfkilot.mongodb.net/?retryWrites=true&w=majority";
 
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+// Creacion MongoCLient con MongoClientOption para setear la API
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
@@ -92,9 +92,9 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
+    // Coneccion cliente-servidor
     await client.connect();
-    // Send a ping to confirm a successful connection
+    // envio de señal para confirmar la correcta ejecución 
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
